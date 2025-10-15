55900b33f8d3cb20157d38be71a549bc009441f7
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-10-15 22:09:30 +0530

    Practiced built-in pipes in Angular

--- src/built-in-pipes/built-in-pipes.ts
+++ src/built-in-pipes/built-in-pipes.ts
@@ -1,19 +1,40 @@
 import { Component } from '@angular/core';
+import { 
+  UpperCasePipe, 
+  LowerCasePipe, 
+  TitleCasePipe, 
+  DatePipe, 
+  CurrencyPipe, 
+  DecimalPipe, 
+  PercentPipe, 
+  SlicePipe, 
+  JsonPipe 
+} from '@angular/common';
 
 @Component({
   selector: 'app-built-in-pipes',
-  imports: [],
+  imports: [
+    UpperCasePipe, 
+    LowerCasePipe, 
+    TitleCasePipe, 
+    DatePipe, 
+    CurrencyPipe, 
+    DecimalPipe, 
+    PercentPipe, 
+    SlicePipe, 
+    JsonPipe
+  ],
   templateUrl: './built-in-pipes.html',
   styleUrl: './built-in-pipes.css'
 })
 export class BuiltInPipes {
 
-  name = 'sony selvaraj';
+  name = 'sony phoebe';
   today = new Date();
   price = 2999.99;
   value = 12345.6789;
   score = 0.875;
   items = ['Angular', 'React', 'Vue', 'Svelte'];
-  user = { name: 'Sony', role: 'Developer', location: 'India' };
+  user = { name: 'Sony', role: 'FullStack Developer', location: 'India' };
 
 }
